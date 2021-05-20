344b2e986bacb0a625f64b18008091456f4f09f1
Jito Chadha <jitochadha@gmail.com>
2021-05-20 15:23:21 -0700

    refactored movie cards into a component

--- client/src/pages/Trending.js
+++ client/src/pages/Trending.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { MovieCards } from "../components/MovieCards";
 
 export const Trending = () => {
     const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=be7a826a76009582fd9bfd917bb48f21";
@@ -9,8 +11,6 @@ export const Trending = () => {
     useEffect(() => {
         const fetchTrending = async () => {
             const res = await (await fetch(url)).json();
-
-            console.log('Res: ', movies, res);
 
             setMovies(res.results);
         };
@@ -27,19 +27,8 @@ export const Trending = () => {
                         <h3>Trending</h3>
                     </Col>
                 </Row>
-                <Row>
-                    {movies && movies.map((movie) =>
-                        <Col key={movie.id} md="4" className="mb-2">
-                            <Card>
-                                <Card.Img variant="top" key={movie.id} src={ "https://image.tmdb.org/t/p/w500" + movie.poster_path } />
-                                <Card.Body>
-                                    <Card.Title>{movie.title}</Card.Title>
-                                </Card.Body>
 
-                            </Card>
-                        </Col>
-                    )}
-                </Row>
+                <MovieCards movies={movies}></MovieCards>
             </Container>
         </>
     )
