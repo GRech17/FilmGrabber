c020465d1626d8365b3757a175052c6fe0636e08
GIR <gireches@gmail.com>
2021-05-20 21:36:48 -0700

    PLAYGROUND WORKING

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,45 @@ const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
 type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveMovie(movieData: MovieInput!): User
     removeMovie(movieId: ID!): User
-}`
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    movieCount: Int
+    savedMovies: [Movie]
+}
+
+type Movie {
+    movieId: ID!
+    overview: String
+    image: String
+    title: String!
+}
+
+input MovieInput {
+    movieId: String
+    image: String
+    title: String!
+
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    
+}
+
+type Auth {
+    token: ID!
+    user: User
+}`;
+
 
 module.exports = typeDefs;
