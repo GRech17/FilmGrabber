b0561ff3fb65176d979b888857339c9dc42d8c2d
Juan Chavez <jmchv2001@gmail.com>
2021-05-20 21:33:58 -0700

    resolvers - typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,55 @@
+const { AuthenticationError } = require("apollo-server-express");
+
 const resolvers = {
     Query: {
-        user:
+        movie: async (parent, { _id }) => {
+            return await movie.findById(_id).populate('category');
+          },
+          user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.products',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+
+    },
+    Mutation: {
+        saveMovie: async (parent, {movieData}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    {_id:context.user._id},
+                    {$push:{savedMovies: movieData}},
+                    {new:true}
+                );
+
+                return updatedUser;
+            }
+
+            // throw new AuthenticationError('You need to be logged in');
+        },
+
+        removeMovie: async (parent, {movieId}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id:context.user._id},
+                    {$pull:{savedMovies:{movieId}}},
+                    {new:true}
+                );
+
+                return updatedUser;
+            }
+
+            // throw new AuthenticationError('You need to be logged in');
+        }
     }
-}
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,30 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
-type Mutation {
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        savedMovies: [Movie]
+    }
+
+    type Movie {
+        _id: ID
+        title: String
+        image: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        movies: (title: String): [Movie]
+
+    }
+    type Mutation {
     saveMovie(movieData: MovieInput!): User
     removeMovie(movieId: ID!): User
 }`
